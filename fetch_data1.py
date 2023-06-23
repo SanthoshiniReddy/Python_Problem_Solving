cf844ec24e31ac3c5e279be846ac68465d282efe
Santhoshini Reddy <santhoshinisingireddy@gmail.com>
2023-06-23 19:46:02 -0400

    Added fetch_data1 file

---
+++ fetch_data1.py
@@ -0,0 +1,15 @@
+import requests
+
+products_url = requests.get("https://dummyjson.com/products")
+products = products_url.json()
+pA = products["products"]
+arrLen = len(pA)
+res = dict()
+for i in range(0, arrLen):
+    category = pA[i]['category']
+    brand = pA[i]['brand']
+    title = pA[i]['title']
+    if brand not in res:
+        res[brand] = []
+    res[brand].append(title)
+res
