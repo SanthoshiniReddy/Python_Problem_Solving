32d634b8e865daf74c3386226c95772ec6e0bda3
Santhoshini Reddy <santhoshinisingireddy@gmail.com>
2023-06-30 20:05:47 -0400

    changed code for even chars

--- str_representation.py
+++ str_representation.py
@@ -2,10 +2,14 @@ def formString(str):
     length = len(str)
     if length <3:
         return("string too short")
-    middle = length // 2
-    if length % 2==0:
-        middle -=1
-    finaltext = str[0] +str[middle] + str[-1]
-    return finaltext
-string = "Shine" 
+    
+    if length % 2 == 0:
+            
+        middle_char = string[length // 2 - 1 : length // 2 + 1]
+    else:
+        middle_char = str[length // 2]
+    
+    formed_string = str[0] + middle_char + str[-1]
+    return formed_string
+string = "dinesh"
 formString(string)
